b6af94669b33205ee086b04c35fc8400147db930
Laird Streak <laird.streak@seequent.com>
2019-01-09 14:00:11 +1300

    Add example of using pytest in place of unittest

---
+++ counting_words/test_cntwords.py
@@ -0,0 +1,7 @@
+from counting_words.count_wordsmodule import count_words
+
+def test_countwords_two():
+    assert count_words('one two',{"one", "two"}) == 2
+
+def test_countwords_three():
+    assert count_words('one two three',{"one", "two", "three"}) == 3
